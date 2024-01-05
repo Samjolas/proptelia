79cbfdde6f18cde02c82e5f98326741d686c121b
Samuel Jolayemi <damoladfrosh@gmail.com>
2024-01-05 09:19:09 +0100

    proptelia

---
+++ gsap.js
@@ -0,0 +1,7 @@
+TweenMax.from ('#hero__text1', {duration:1, y:30, opacity:0 });
+TweenMax.from ('#hero__text2', {duration:1, y:30, opacity:0, delay: .6 });
+TweenMax.from ('#hero__text3', {duration:1, y:30, opacity:0, delay: 1.1 });
+TweenMax.from ('#hero__text4', {duration:1, y:30, opacity:0, delay: 1.4 });
+TweenMax.from ('#hero__btn', {duration:1, y:15, opacity:0, delay: 1.7 });
+
+TweenMax.from ('.hero__sub', {duration:2, x:100, opacity:0,  delay: 2, ease:'Expo.easeInOut'});
